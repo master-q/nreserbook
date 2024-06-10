dc8a5ae6f39343f618ff59fcc7dc75fa4b640301
Kiwamu Okabe <kiwamu@gmail.com>
2024-06-10 12:20:48 +0900

    Parse JSON

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 extern crate reqwest;
-use std::io;
+use serde::{Deserialize, Serialize};
+use serde_json::{Map, Value};
+
+#[derive(Serialize, Deserialize)]
+struct CalilCheck {
+    session: String,
+    r#continue: i32,
+    books: Map<String, Value>,
+}
 
 const APPKEY: &str = env!("CALIL_APPKEY");
 
 fn main() {
-    let mut res = reqwest::blocking::get(
+    let res = reqwest::blocking::get(
         "https://api.calil.jp/check?appkey".to_string()
             + APPKEY
             + "&isbn=4405012539,4492553908&systemid=Kanagawa_Yokohama&format=json&callback=no"
-    ).unwrap();
-    res.copy_to(&mut io::stdout()).unwrap();
+    ).unwrap().text().unwrap();
+    let json: CalilCheck = serde_json::from_str(&res).unwrap();
+    println!("{}", res);
+
+    println!("session={}, c={}", json.session, json.r#continue);
+    let iter = json.books.iter();
+    for val in iter {
+        let yokohama = val.1.as_object().unwrap()["Kanagawa_Yokohama"].as_object().unwrap();
+        println!("{}: s={}, r={}", val.0, yokohama["status"], yokohama["reserveurl"],);
+    }
 }
